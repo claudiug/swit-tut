3e8dccfdfa725c057a35414e4bb149cfa19fa18c
claudiu <claudiu.garba@gmail.com>
2014-07-27 13:01:39 +0200

    lol

--- swift-tut/main.swift
+++ swift-tut/main.swift
@@ -10,3 +10,23 @@ import Foundation
 
 println("Hello, World!")
 
+var name = "this is a string "
+let nr = 12 //constant
+
+println(name)
+println(nr)
+
+var myArray = []
+
+println(myArray.count)
+
+
+var second = [
+    1: "one",
+    2: "second"
+]
+
+println(second[1])
+
+var ee  = Dictionary<String, Int>()
+println(ee.generate())

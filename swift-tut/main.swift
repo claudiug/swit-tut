b9b7838c5114a8087f34efebaecb1ab82b98194f
claudiu <claudiu.garba@gmail.com>
2014-07-27 13:14:52 +0200

    hmm

--- swift-tut/main.swift
+++ swift-tut/main.swift
@@ -30,3 +30,74 @@ println(second[1])
 
 var ee  = Dictionary<String, Int>()
 println(ee.generate())
+
+let individualScores = [75, 43, 103, 87, 12]
+
+for x in individualScores {
+    if x  > 50 {
+        println(x)
+    }else{
+        println(x) //lol
+    }
+}
+var greet = "hello"
+var optional: String? = "Bau"
+
+if let name = optional {
+    greet = "Hello \(name)"
+}
+
+
+println(greet)
+
+
+protocol ExampleProtocol {
+    var simple: String {get}
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simple = "This is a very simple example"
+
+    func adjust() {
+        println(simple)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

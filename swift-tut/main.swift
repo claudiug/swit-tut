2f7ea0ff60a5b1139bc55bacfe049c34c8bf6857
claudiu <claudiu.garba@gmail.com>
2014-07-27 12:47:38 +0200

    init commit

---
+++ swift-tut/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-tut
+//
+//  Created by claudiu on 7/27/14.
+//  Copyright (c) 2014 com.demo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
